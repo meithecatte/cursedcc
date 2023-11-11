608ca8664dabd496296b3f6a2073cc4720eefa17
Maja Kądziołka <maya@compilercrim.es>
2023-11-11 21:41:14 +0100

    Emit short jumps if possible

--- jumps.sh
+++ jumps.sh
@@ -42,17 +42,87 @@ jump() {
     code+="\x0f\x0b" # ud2, also the size of all short jumps
 }
 
+# Helper routines for resolve_jumps. Isn't it neat how bash uses dynamic scope?
+#
+# The algorithm in use here was inspired by https://arxiv.org/abs/0812.4973
+
+# mark_long jump_index
+mark_long() {
+    local -i i=$1
+    if (( !jump_is_long[i] )); then
+        jump_is_long[i]=1
+        jump_queue+=("$i")
+    fi
+}
+
+consider_jump() {
+    local -i disp=jump_cur_disp[$1]
+    if (( disp < -128 || 127 < disp )); then
+        mark_long $1
+    fi
+}
+
+long_encoding_size() {
+    if [ -z "${jump_condition[$1]-}" ]; then
+        res=5 # e9, then four bytes of offset
+    else
+        res=6 # 0f, 8x, then four bytes of offset
+    fi
+}
+
 resolve_jumps() {
     # HACK: It helps a lot with the loops below if we can be sure that after
     # the last label, there are no jumps.
     label __end_of_function
 
-    local -ia jump_is_long=()
+    # A lower bound for the distance the jump will need to cover.
+    # If the jump is currently not marked as long, this is the accurate
+    # displacement value, assuming the long jumps are exactly the ones that
+    # have been dequeued and processed already.
+    local -ia jump_cur_disp=()
+    local -ia jump_is_long=() jump_queue=()
     local -i i
 
-    # For now, conservatively assume all jumps need the long encoding.
-    for (( i=0; i <= ${#jump_position[@]}; i++ )); do
-        jump_is_long[i]=1
+    for (( i=0; i < ${#jump_position[@]}; i++ )); do
+        local -i target=${label_position["${jump_target[i]}"]}
+        jump_cur_disp[i]=target-jump_position[i]-2
+        consider_jump $i
+    done
+
+    while (( ${#jump_queue[@]} )); do
+        # Technically not a FIFO queue but this is immaterial
+        local -i j=${#jump_queue[-1]}; unset jump_queue[-1]
+
+        # Jumps at lower addresses
+        for (( i=j-1; i >= 0 \
+            && jump_position[j] - jump_position[i] < 128; i-- ))
+        do
+            (( jump_is_long[i] )) && continue
+
+            local -i target=${label_position["${jump_target[i]}"]}
+            if (( target > jump_position[j] )); then
+                long_encoding_size $i
+                jump_cur_disp[i]+=res-2
+                consider_jump $i
+            fi
+        done
+
+        # Jumps at higher addresses
+        # 
+        # (yes, this should be < 128, not <= 128 as they say in the paper.
+        # consider the boundary case and it'll be clear to you)
+        for (( i=j+1; i < ${#jump_position[@]} \
+            && jump_position[i] - jump_position[j] < 128; i++ ))
+        do
+            (( jump_is_long[i] )) && continue
+
+            local -i target=${label_position["${jump_target[i]}"]}
+            if (( target <= jump_position[j] )); then
+                long_encoding_size $i
+                jump_cur_disp[i]-=res-2
+                consider_jump $i
+            fi
+        done
     done
 
     # Expand the spacing to fit all the widened jumps.
@@ -67,10 +137,8 @@ resolve_jumps() {
 
             if (( !jump_is_long[i] )); then
                 jump_size=2
-            elif [ -z "${jump_condition[i]-}" ]; then
-                jump_size=5 # e9, then four bytes of offset
             else
-                jump_size=6 # 0f, 8x, then four bytes of offset
+                long_encoding_size $i; jump_size=$res
             fi
 
             jump_actual_position[i]=jump_position[i]+adjust
@@ -90,23 +158,40 @@ resolve_jumps() {
         local -i target="${label_actual_position["${jump_target[i]}"]}"
         local -i position="${jump_actual_position[i]}"
         local patch=""
+        local -i disp
         if (( jump_is_long[i] )); then
             if [ -z "${jump_condition[i]-}" ]; then
                 patch+="\xe9"
-                p32 patch $((target - position - 5))
+                disp=$((target - position - 5))
             else
                 patch+="\x0f"
                 p8 patch $((0x80 + jump_condition[i]))
-                p32 patch $((target - position - 6))
+                disp=$((target - position - 6))
             fi
+
+            if (( -128 <= disp && disp < 128 )); then
+                internal_error "jump unnecessarily marked as long"
+                end_diagnostic
+                exit 1
+            fi
+
+            p32 patch $disp
         else
-            if [ -z "${jump_condition[-]-}" ]; then
+            if [ -z "${jump_condition[i]-}" ]; then
                 patch+="\xeb"
-                p8 patch $((target - position - 2))
+                disp=$((target - position - 2))
             else
                 p8 patch $((0x70 + jump_condition[i]))
-                p8 patch $((target - position - 2))
+                disp=$((target - position - 2))
             fi
+
+            if ! (( -128 <= disp && disp < 128 )); then
+                internal_error "jump displacement too long for a short jump"
+                end_diagnostic
+                exit 1
+            fi
+
+            p8 patch $disp
         fi
 
         patch_position+=("${jump_position[i]}")
